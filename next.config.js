54a6c7774e64790be40c87d01f091e28701562f8
Dan Burzo <danburzo@gmail.com>
2019-06-17 18:49:06 +0300

    More configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const isProd = (process.env.NODE_ENV || 'production') === 'production';
+
 const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
 	exportPathMap: function() {
 		return {
 			'/': { page: '/' }
 		};
-	}
+	},
+	assetPrefix: isProd ? '/webcolors-cymk' : ''
 });
