b497561b55c38307d4498aa2ddefa3cbdb923a1f
Dan Burzo <danburzo@gmail.com>
2019-06-18 11:52:10 +0300

    Use node-lcms

--- tools/transform.js
+++ tools/transform.js
@@ -1,6 +1,6 @@
-let transicc = require('./util/transform-icc');
 let culori = require('culori');
 let fs = require('fs');
+let { convert } = require('node-lcms');
 
 // CSS Named Colors
 let COLORS = Object.keys(culori.colorsNamed)
@@ -9,9 +9,6 @@ let COLORS = Object.keys(culori.colorsNamed)
 
 // CMYK Profiles
 const PROFILES = ['pso-coated-v3', 'pso-uncoated-v3-fogra52'];
-
-// RGB Profile to use
-const RGB_PROFILE = '*srgb';
 
 const hex = culori.formatter('hex');
 
@@ -29,52 +26,15 @@ const arr_to_hex = arr => {
 	});
 };
 
-const convertColor = (color, cmyk_profile = 'cmyk') =>
-	new Promise((resolve, reject) => {
-		// prepare RGB color for transicc()
-		let rgb_arr = rgb_to_arr(color);
-
-		transicc(RGB_PROFILE, cmyk_profile, rgb_arr, (err, res) => {
-			if (err) {
-				reject(err);
-				return;
-			}
-
-			// transicc() will return a result array
-			// of more than 4 items, which breaks the
-			// subsequent call. Trim it down.
-			let cmyk_arr = res.slice(0, 4);
-
-			transicc(cmyk_profile, RGB_PROFILE, cmyk_arr, (err, res) => {
-				if (err) {
-					reject(err);
-					return;
-				}
-
-				resolve({
-					original: color,
-					converted: arr_to_hex(res)
-				});
-			});
-		});
-	});
-
-const convert = (colors, profile) =>
-	colors.reduce(
-		(chain, color) =>
-			chain.then(res =>
-				convertColor(color, profile).then(color => [...res, color])
-			),
-		Promise.resolve([])
-	);
+const convertColor = (original, cmyk_profile) => {
+	let profile_path = require.resolve(`../profiles/${cmyk_profile}.icc`);
+	let rgb_to_cmyk = convert({ profile_out: profile_path });
+	let cmyk_to_rgb = convert({ profile_in: profile_path });
+	let converted = arr_to_hex(cmyk_to_rgb(rgb_to_cmyk(rgb_to_arr(original))));
+	return { original, converted };
+};
 
 PROFILES.forEach(profile => {
-	convert(COLORS, profile)
-		.then(res =>
-			fs.writeFileSync(
-				`data/${profile}.json`,
-				JSON.stringify(res, null, 2)
-			)
-		)
-		.catch(err => console.error(err));
+	let res = COLORS.map(color => convertColor(color, profile));
+	fs.writeFileSync(`data/${profile}.json`, JSON.stringify(res, null, '\t'));
 });
